91c7b48e096bc7fdc8ea6d41761a320dc49cbead
boge <boge@boge.com>
2022-07-01 11:12:30 +0800

    新增hello方法

--- src/main/java/com/jiyun/demo1/web/HelloController.java
+++ src/main/java/com/jiyun/demo1/web/HelloController.java
@@ -3,7 +3,7 @@ package com.jiyun.demo1.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public class HelloController {
-    @RequestMapping("/")
+    @RequestMapping("/1")
     public String heloo(){
         return "hello";
     }
